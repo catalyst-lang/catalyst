248193ed4e0ffb7be11c969c65ceb12657e8de44
Bas du Pré <me@basdupre.com>
2023-02-17 22:34:45 +0100

    Update runtime.cpp

--- toolchain/compiler/src/runtime.cpp
+++ toolchain/compiler/src/runtime.cpp
@@ -64,13 +64,19 @@ void runtime::insert_function(const char* name, llvm::JITTargetAddress target, c
     functions[name] = target;
 }
 
+void* my_malloc(uint64_t size) {
+	return malloc(size);
+}
+
 llvm::FunctionCallee runtime::get_malloc() {
 	auto m = this->state->TheModule->getOrInsertFunction(
-		"malloc", 
+		"my_malloc", 
 		llvm::AttributeList::get(*state->TheContext, 0, { llvm::Attribute::NoAlias }),
 		llvm::PointerType::get(*state->TheContext, 0), 
 		llvm::IntegerType::get(*state->TheContext, sizeof(size_t) * 8)
 	);
+
+	functions["my_malloc"] = llvm::pointerToJITTargetAddress(&my_malloc);
 
 	return m;
 }
